2c57281ed2952c5c183b14594fd520d0cf206a1b
neotango7614 <neotango7614@gmail.com>
2025-11-07 17:12:01 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from langchain.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
+import streamlit as st
+
+# llm = OpenAI()
+# result = llm.predict("hi!")
+# print(result)
+
+# load_dotenv()
+
+chat_model = ChatOpenAI()
+
+content = "코딩"
+
+result = chat_model.predict(content + "에 대한 시를 써줘")
+print(result)
+
+st.title('인공지능 시인')
+title = st.text_input('시의 주제를 제시해 주세요')
+if st.button("시 작성 요청"):
+    with st.spinner('Wait for it'):
+        result = chat_model.predict(title + "에 대한 시를 써줘")
+        st.write(result)
